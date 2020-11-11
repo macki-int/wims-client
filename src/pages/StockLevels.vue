3e9bc1ee0c1788f8507a1fc7f88304ef93de953c
macki-int <macki-int@users.noreply.github.com>
2020-11-12 00:53:51 +0100

    add dynamic title in table StockLevels

--- src/pages/StockLevels.vue
+++ src/pages/StockLevels.vue
@@ -4,15 +4,15 @@
     <div class="row">
         <div class="col-md-6">
             <div class="q-pa-md">
-                <q-table title="" :data=" data" :columns="columns" row-key="name" />
+                <q-table :title="$route.params.id" :data=" data" :columns="columns" row-key="name" />
             </div>
         </div>
 
         <div class="col-3"></div>
 
         <div class=" col-3">
-            <p>Typy produktów:
-                {{ $route.params.id }}
+            <p>Szczegóły produktu:
+
             </p>
         </div>
     </div>
