3772277b602b6669bbe43f0949d9b59bd707bcd8
Marek <mar.jan@int.pl>
2020-11-15 07:13:18 +0100

    remove copy StockLevels.vue, add table Products in StockLevels

--- src/pages/StockLevels.vue
+++ src/pages/StockLevels.vue
@@ -1,15 +1,14 @@
 <template>
     <q-page class="flex flex-left">
-
         <div class="q-pa-md">
             <div class="row">
-                <div class="col-6-md">
+                <div class="col-6-md q-pr-md">
                     <q-card class="my-card" style="width: 1000px">
                         <div style="min-height: 700px">
                             <q-card-section>
                                 <div class="text-h6">{{ productTypeName.name }}</div>
                             </q-card-section>
-                            <q-markup-table dense>
+                            <q-markup-table dense class="no-shadow">
                                 <thead>
                                     <tr>
                                         <th class="text-left">Id</th>
@@ -20,50 +19,45 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr v-for="(data, index) in products" :key="index">
-                                        <td class="text-left">{{ data.id }}</td>
-                                        <td class="text-right">{{ data.employee_salary }}</td>
-                                        <td class="text-right">{{ data.employee_age }}</td>
-                                        <td class="text-right">{{ data.profile_image }}</td>
+                                    <tr v-for="(product, id) in products" :key="id">
+                                        <td class="text-left">{{ product.id }}</td>
+                                        <td class="text-left">{{ product.name }}</td>
+                                        <!--td class="text-right">{{ product.width }}</td>
+                                                    <td class="text-right">{{ product.lenght }}</td>
+                                                    <td class="text-right">{{ product.quantity }}</td-->
                                     </tr>
                                 </tbody>
-
                             </q-markup-table>
-
                         </div>
                     </q-card>
                 </div>
 
-                <div class="col-1-md" style="width: 10px">
-                </div>
-                <div class="col-3-md" style="width: 500px ">
-
-                    <div class="my-card">
-
-                        <q-card class="my-card">
-                            <div style="min-height: 700px">
-                                <q-card-section>
-                                    <div class="text-h6">{{ nameProduct }}</div>
-                                </q-card-section>
-                            </div>
-                        </q-card>
-                    </div>
-
+                <div class="col-3-md" style="width: 500px">
+                    <q-card class="my-card">
+                        <div style="min-height: 700px">
+                            <q-card-section>
+                                <div class="text-h6">{{ nameSelectedProduct }}</div>
+                            </q-card-section>
+                        </div>
+                    </q-card>
                 </div>
             </div>
         </div>
-
-
-
     </q-page>
 </template>
 
 <script>
 import axios from "axios";
-// import VueRouter from "vue-router";
 
 export default {
     name: "StockLevels",
+
+    watch: {
+        $route(to, from) {
+            this.getProductTypeName();
+            this.getProductsByProductTypeId();
+        }
+    },
 
     mounted() {
         this.getProductTypeName();
@@ -75,30 +69,9 @@ export default {
         return {
             productTypeName: [],
             products: [],
-            nameProduct: ''
+            nameSelectedProduct: ''
         }
     },
-
-    // afterRouteUpdate(to, from, next) {
-
-    // this.productTypeName = null;
-    // pretendGet((err, name) => {
-    //     this.setName(err, name);
-    // next();
-    // });
-    // },
-    // routes: [{
-    //     router.beforeEach((to, from, next) => {
-    //         alert($route.params.id)
-    //     })
-    // }],
-
-    watch: {
-        $route(to, from) {
-            this.getProductTypeName();
-        }
-    },
-
 
     methods: {
         getProductTypeName: function() {
@@ -110,7 +83,6 @@ export default {
                 })
                 .then(response => {
                     this.productTypeName = response.data;
-                    // nameProduct = this.productTypeName.name;
                     // alert(event.target.tagName);
                     //console.log("response: " + JSON.stringify(response.data));
                 })
@@ -122,31 +94,26 @@ export default {
                         icon: "report_problem"
                     });
                 });
-            // console.log("refresh");
         },
 
         getProductsByProductTypeId: function() {
-            const url = "http://localhost:8080/products/" + this.$route.params.id;
+            const url = "http://localhost:8080/products/product-types/" + this.$route.params.id;
             axios
                 .get(url, {
                     dataType: "json",
                     headers: {}
                 })
                 .then(response => {
-                    this.productTypeName = response.data;
-                    // nameProduct = this.productTypeName.name;
-                    // alert(event.target.tagName);
-                    //console.log("response: " + JSON.stringify(response.data));
+                    this.products = response.data;
                 })
                 .catch(() => {
                     this.$q.notify({
                         color: "negative",
                         position: "top",
-                        message: "Product type loading failed",
+                        message: "Products loading failed",
                         icon: "report_problem"
                     });
                 });
-            // console.log("refresh");
         }
     }
 };
