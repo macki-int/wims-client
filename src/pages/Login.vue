93a6c5b4c4bc6ee383f3b6fb6c44837abf9d2baf
macki-int <macki@int.pl>
2021-01-22 00:21:25 +0100

    add logo to login form

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,19 +1,21 @@
 <template>
 <q-page class="flex flex-left">
-    <q-card class="my-card">
-        <q-form v-on:submit.prevent="login" class="q-gutter-md">
-            <q-card-section>
-                <div class="text-primary">Logowanie:</div>
-            </q-card-section>
-            <q-card-section>
-                <q-input required v-model="user" no-outline type="text" label="Nazwa użytkownika" />
-                <q-input required v-model="password" no-outline type="password" label="Hasło" />
-            </q-card-section>
-            <q-card-actions align="right" class="text-primary">
-                <q-btn flat type="submit" label="Zaloguj" color="primary" />
-            </q-card-actions>
-        </q-form>
-
+    <q-card class="my-card" style="width: 500px; height: 400px">
+        <q-card-section>
+            <q-form v-on:submit.prevent="login" class="q-gutter-md">
+                <q-card-section>
+                    <img src="~assets/wims-logo-full.svg" style="height: 50px;" />
+                    <div class="text-h6 text-primary">Logowanie:</div>
+                </q-card-section>
+                <q-card-section>
+                    <q-input required v-model="user" no-outline type="text" clearable label="Nazwa użytkownika" />
+                    <q-input required v-model="password" no-outline type="password" clearable label="Hasło" />
+                </q-card-section>
+                <q-card-actions align="left" class="text-primary">
+                    <q-btn flat type="submit" label="Zaloguj" color="primary" />
+                </q-card-actions>
+            </q-form>
+        </q-card-section>
     </q-card>
 
 </q-page>
