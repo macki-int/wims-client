ee96b21db38d7b25675f4bbf5ecec0cb29e3ff45
macki-int <macki@int.pl>
2021-01-21 22:37:09 +0100

    add route path /Login.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,32 +1,33 @@
-const routes = [{
-        path: '/',
-        component: () =>
-            import ('layouts/MainLayout.vue'),
-        children: [{
-                path: '',
-                component: () =>
-                    import ('pages/Index.vue')
-            },
-            {
-                path: 'products',
-                component: () =>
-                    import ('pages/Products.vue')
-            },
-            {
-                path: '/:id?',
-                component: () =>
-                    import ('pages/StockLevels.vue')
-            }
-        ]
-    },
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/Index.vue")
+      },
+      {
+        path: "products",
+        component: () => import("pages/Products.vue")
+      },
+      {
+        path: "login",
+        component: () => import("pages/Login.vue")
+      },
+      {
+        path: "/:id?",
+        component: () => import("pages/StockLevels.vue")
+      }
+    ]
+  },
 
-    // Always leave this as last one,
-    // but you can also remove it
-    {
-        path: '*',
-        component: () =>
-            import ('pages/Error404.vue')
-    }
-]
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  }
+];
 
-export default routes
+export default routes;
