9d0ce1035a0bee6459f7e662ceb8def5a0d070bf
macki-int <macki@int.pl>
2021-03-01 00:43:04 +0100

    set loggedUser value from localStorage in mouted function

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -92,6 +92,7 @@ export default {
 
         if (localStorage.getItem("token")) {
             this.auth = "logged";
+            this.loggedUser = localStorage.getItem("userName");
             this.getProductTypes();
         };
 
@@ -208,8 +209,8 @@ export default {
         showInfoDialog: function () {
             this.$q
                 .dialog({
-                    title: "<span class=text-primary><strong>WIMS</strong> v0.02(beta)",
-                    message: "<img src='~assets//wims-logo-full.svg'><span class=text-primary><strong>Warehouse Inventory Management System</strong>" +
+                    title: "<span class=text-primary><strong>WIMS</strong> v0.03(beta)",
+                    message: "<span class=text-primary><strong> Warehouse Inventory Management System</strong>" +
                         "<br/>" +
                         "TROLL-Systems Marek Janicki (C)" +
                         "<br/>" +
