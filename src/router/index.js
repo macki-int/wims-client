66224460fc85eee9dea113f803cd5c84564188e3
Marek <macki@int.pl>
2021-03-07 14:50:44 +0100

    remove empty lines

--- src/router/index.js
+++ src/router/index.js
@@ -12,13 +12,6 @@ Vue.use(VueRouter);
 Vue.prototype.$API_URL = "https://wims-mj.herokuapp.com/";
 // Vue.prototype.$API_URL = "https://v1-wims.herokuapp.com/"
 
-Vue.prototype.$loggedUser = {
-  username: "",
-  firstName: "",
-  lastName: "",
-  role: "",
-  active: ""
-};
 /*
  * If not building with SSR mode, you can
  * directly export the Router instantiation;
@@ -28,17 +21,17 @@ Vue.prototype.$loggedUser = {
  * with the Router instance.
  */
 
-export default function(/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
+export default function( /* { store, ssrContext } */ ) {
+    const Router = new VueRouter({
+        scrollBehavior: () => ({ x: 0, y: 0 }),
+        routes,
 
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  });
+        // Leave these as they are and change in quasar.conf.js instead!
+        // quasar.conf.js -> build -> vueRouterMode
+        // quasar.conf.js -> build -> publicPath
+        mode: process.env.VUE_ROUTER_MODE,
+        base: process.env.VUE_ROUTER_BASE
+    });
 
-  return Router;
+    return Router;
 }
