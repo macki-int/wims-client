d1c7b95164f32d9322111c917d0dd610276cfd24
macki-int <macki@int.pl>
2021-04-13 15:23:09 +0200

    add icon table for inventory with description

--- src/pages/StockLevels.vue
+++ src/pages/StockLevels.vue
@@ -23,7 +23,8 @@
                         <q-tr class="cursor-pointer" slot="body" slot-scope="props" :props="props" @click.native="onRowClick(props.row, props)">
                             <q-td key="product" :props="props">
                                 {{ props.row.inventory.product.name }}
-                                <q-icon v-if="props.row.reservationCounter>0" class="q-pr-md text-weight-bolder" color="primary" size="16px" name="schedule" />
+                                <q-icon v-if="props.row.reservationCounter>0" class="text-weight-bolder" color="primary" size="16px" name="schedule" />
+                                  <q-icon v-if="props.row.inventory.description.length>0" class="text-weight-bolder" color="primary" size="16px" name="notes" />
                             </q-td>
                             <q-td key="productWidth" :props="props" :class="props.row.inventory.mainDimension?'text-primary text-bold':''">
                                 {{ setNumericFormat(props.row.inventory.productWidth) }}
