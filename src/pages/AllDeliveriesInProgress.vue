7d0069804b783637c45d0556c6ea9abab33313f4
macki-int <macki@int.pl>
2021-10-07 23:19:35 +0200

    add AllDeliveriesInProgress page

---
+++ src/pages/AllDeliveriesInProgress.vue
@@ -0,0 +1,24 @@
+<template>
+<q-page class="flex flex-left">
+    <div class="q-pa-md">
+        <q-card class="my-card fit" style="min-width: 60vw; min-height: 75vh">
+            <q-card>
+            </q-card>
+        </q-card>
+    </div>
+</q-page>
+</template>
+
+<script>
+import axios from "axios";
+import { setNumericFormat } from "../js/setNumericFormat.js";
+import LoggedUserFromLocalStore from "../js/LoggedUserFromLocalStore.js"
+
+export default {
+    name: "AllDeliveriesInProgress",
+
+    data() {
+
+    },
+}
+</script>
