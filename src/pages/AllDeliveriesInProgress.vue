cb3dde61b10225f308f8ab154d557c0885c60f90
macki-int <macki@int.pl>
2021-10-07 23:24:02 +0200

    add table Lista aktualnych dostaw in AllDeliveriesInProgress page

--- src/pages/AllDeliveriesInProgress.vue
+++ src/pages/AllDeliveriesInProgress.vue
@@ -3,6 +3,12 @@
     <div class="q-pa-md">
         <q-card class="my-card fit" style="min-width: 60vw; min-height: 75vh">
             <q-card>
+                <q-table dense flat :data="deliveries" :columns="columns" row-key="name" :filter="filter" :pagination.sync="pagination" hide-no-data color="primary" v-bind:request="getDeliveriesInProgress">
+                    <template slot="top-left">
+                        <div class="q-pa-sm text-h6 text-primary">
+                            Lista aktualnych dostaw
+                        </div>
+                    </template>
             </q-card>
         </q-card>
     </div>
