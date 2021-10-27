8d89b58145b831e6f7cf5cf644ad2e3d40cec758
macki-int <macki@int.pl>
2021-10-27 14:52:24 +0200

    add ServerVersion component

---
+++ src/components/ServerVersion.vue
@@ -0,0 +1,61 @@
+<template>
+<q-dialog v-model="card" ref="dialog" >
+    <q-card class="my-card text-primary" style="min-width:400px">
+        <div class="row">
+            <img alt="Wims logo" src="~assets/icon-96x96.png" style="width: 23%;">
+            <q-card-section >
+                <div class="text-h4 ellipsis">
+                    <strong>WIMS</strong>
+                </div>
+                <div class="text-caption">
+                    Warehouse Inventory Management System
+                </div>
+            </q-card-section>
+        </div>
+        <q-card-section class="bg-primary text-white">
+            TROLL-Systems Marek Janicki &copy; 2021
+        </q-card-section>
+        <q-card-actions>
+            <div class="col q-ml-sm text-caption" align="left">
+               Wersja serwera: &beta;
+            </div>
+            <div class="col" align="right">
+                <q-btn v-close-popup flat color="primary" label="OK" />
+            </div>
+        </q-card-actions>
+    </q-card>
+</q-dialog>
+</template>
+
+<script>
+import LoggedUserFromLocalStore from "../js/LoggedUserFromLocalStore.js"
+
+export default {
+    name: "ServerVersion",
+
+    mounted: function () {
+        this.loggedUser = LoggedUserFromLocalStore.getLoggedUserFromLocalStore();
+    },
+
+    data() {
+        return {
+            card: false,
+            infoRole: "",
+            loggedUser: {
+                username: "",
+                firstName: "",
+                lastName: "",
+                role: "",
+                active: ""
+            },
+        }
+    },
+
+    methods: {
+        show: function () {
+            this.infoRole = this.loggedUser.role.split('_')
+            this.$refs.dialog.show()
+        }
+    }
+}
+</script>
