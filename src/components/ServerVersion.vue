5cd7bb7e74ed8c11e8ad5807d271e29bf4422418
Marek <macki@int.pl>
2021-10-28 22:57:04 +0200

    add function getServerVersion

--- src/components/ServerVersion.vue
+++ src/components/ServerVersion.vue
@@ -1,9 +1,9 @@
 <template>
-<q-dialog v-model="card" ref="dialog" >
+<q-dialog v-model="card" ref="dialog">
     <q-card class="my-card text-primary" style="min-width:400px">
         <div class="row">
             <img alt="Wims logo" src="~assets/icon-96x96.png" style="width: 23%;">
-            <q-card-section >
+            <q-card-section>
                 <div class="text-h4 ellipsis">
                     <strong>WIMS</strong>
                 </div>
@@ -17,7 +17,7 @@
         </q-card-section>
         <q-card-actions>
             <div class="col q-ml-sm text-caption" align="left">
-               Wersja serwera: &beta;
+                Wersja serwera: <strong>{{ infoServerVersion }}</strong>
             </div>
             <div class="col" align="right">
                 <q-btn v-close-popup flat color="primary" label="OK" />
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import LoggedUserFromLocalStore from "../js/LoggedUserFromLocalStore.js"
 
 export default {
@@ -35,12 +36,16 @@ export default {
 
     mounted: function () {
         this.loggedUser = LoggedUserFromLocalStore.getLoggedUserFromLocalStore();
+        this.getServerVersion();
     },
 
     data() {
         return {
             card: false,
+
             infoRole: "",
+            infoServerVersion: "",
+
             loggedUser: {
                 username: "",
                 firstName: "",
@@ -52,6 +57,39 @@ export default {
     },
 
     methods: {
+        getServerVersion: function () {
+            const url = this.$API_URL + "server-versions";
+
+             axios
+                .get(url, {
+                    contentType: "application/json",
+                    dataType: "json",
+                    headers: { "Authorization": localStorage.getItem("token") }
+                })
+                .then((response) => {
+                    this.infoServerVersion = response.data;
+                })
+                .catch((error) => {
+                    if (error.response.status === 403) {
+                        this.$q.notify({
+                            color: "negative",
+                            position: "top",
+                            message: "Nie jesteś zalogowany",
+                            icon: "report_problem",
+                        });
+                        this.$router.push("/login")
+                    } else {
+                        this.$q.notify({
+                            color: "negative",
+                            position: "top",
+                            message: "Błąd pobierania wersji serwera",
+                            icon: "report_problem",
+                        });
+                    };
+                });
+
+        },
+
         show: function () {
             this.infoRole = this.loggedUser.role.split('_')
             this.$refs.dialog.show()
