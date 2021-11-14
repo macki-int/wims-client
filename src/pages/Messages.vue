55d1b21ad37e3f99f3993759fe226d7896c4c767
macki-int <macki@int.pl>
2021-11-14 19:51:50 +0100

    add message.vue page

---
+++ src/pages/Messages.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+import axios from "axios";
+import LoggedUserFromLocalStore from "../js/LoggedUserFromLocalStore.js"
+
+export default {
+    name: "Messages",
+
+    mounted() {
+        this.loggedUser = LoggedUserFromLocalStore.getLoggedUserFromLocalStore();
+        
+    },
+
+    data() {
+        
+    }
+}
+</script>
